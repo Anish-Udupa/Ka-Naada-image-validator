d21ccdc49663af8db74bee9644ebe264de4915e5
Anish-Udupa <udupa.anish@gmail.com>
2022-08-21 18:44:22 +0530

    Added translation and rotation function

--- script.py
+++ script.py
@@ -1,14 +1,57 @@
 # If the library is not present, install it with the command:
 # pip install opencv-python
 import cv2 as cv
+import numpy as np
+
+def align_images(img1, img2):
+    height, width = img1.shape[:2]
+    center = (width//2, height//2)
+
+    # Syntax = (start, stop, step) step doesnt work. Make it work
+    # Finetune these values
+    x_range = (-2, 2, 0.1)   # Step of 0.1
+    y_range = (-2, 2, 0.1)   # Step of 0.1
+    rotation_range = (-1, 1, 0.1)    # Step of 0.1
+
+    rotate_matrix = cv.getRotationMatrix2D(center=center, angle=1, scale=1)
+    rotated_img = cv.warpAffine(src=img2, M=rotate_matrix, dsize=(width, height))
+
+    # cv.imshow("rotated image OR", cv.bitwise_or(rotated_img, img1))
+
+    # x = 5
+    # y = 5
+    # translation_matrix = np.array([
+    #     [1, 0, x],
+    #     [0, 1, y]
+    # ], dtype=np.float32)
+    # translated_image = cv.warpAffine(src=img2, M=translation_matrix, dsize=(width, height))
+    # cv.imshow("translated image OR", cv.bitwise_or(translated_image, img1))
+
+    for x in range(x_range[0], x_range[1]):
+        for y in range(y_range[0], y_range[1]):
+            translation_matrix = np.array([
+                [1, 0, x],
+                [0, 1, y]
+            ], dtype=np.float32)
+            translated_image = cv.warpAffine(src=img2, M=translation_matrix, dsize=(width, height))
+            for angle in range(rotation_range[0], rotation_range[1]):
+                rotate_matrix = cv.getRotationMatrix2D(center=center, angle=angle, scale=1)
+                rotated_image = cv.warpAffine(src=translated_image, M=rotate_matrix, dsize=(width, height))
+                bitwise_or_img = cv.bitwise_or(img1, rotated_image)
+                # if np.allclose(bitwise_or_img, img1):
+                #     cv.imshow("Match", bitwise_or_img)
+                #     print(f"x={x}, y={y}, angle={angle}")
+                #     return
+                cv.imshow(f"x={x}, y={y}, angle={angle}", bitwise_or_img)
+
 
 # Reading the images
 cap1 = cv.imread("./images/good.jpg")
 cap2 = cv.imread("./images/bad.jpg")
 
 # Displaying the read images
-cv.imshow("Original 1", cap1)
-cv.imshow("Original 2", cap2)
+# cv.imshow("Original 1", cap1)
+# cv.imshow("Original 2", cap2)
 
 # Converting image to grayscale
 img1 = cv.cvtColor(cap1, cv.COLOR_BGR2GRAY)
@@ -36,6 +79,8 @@ cv.imshow("Filtered AND", cv.bitwise_and(img1, img2))
 
 # Displaying the image formed by performing bitwise AND on the two samples
 cv.imshow("Filtered OR", cv.bitwise_or(img1, img2))
+
+align_images(img1, img2)
 
 # cv.imshow("Filtered XOR1", cv.bitwise_xor(img1, img2))
 # cv.imshow("Filtered XOR2", cv.bitwise_xor(img2, img1))
